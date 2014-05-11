d2a2d65148021be9aff56a9c979ffe50ff29e784
Sam Vevang <sam.vevang@gmail.com>
2014-05-11 17:25:50 -0400

    point home action to -> '/'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home_controller/home'
+  get '/', to: 'home_controller#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
