f490dfd1dc4af49bd98cc31cc11eeec262307d94
Sam Vevang <sam.vevang@gmail.com>
2017-03-08 09:41:59 -0600

    fine tune db create script

--- lib/create_puerto_rico_db.sh
+++ lib/create_puerto_rico_db.sh
@@ -5,8 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git submodule init && git submodule update;
 
 cd $DIR/osm-extract-by-region;
+
+echo  " * Extracting osm region for puerto rico:"
 ./osm_extract.sh central-america puerto-rico
 
-createdb puerto_rico_osm
-echo "create extension postgis" | psql puerto_rico_osm
-osm2pgsql  -d puerto_rico_osm extractions/puerto-rico.osm
+psql -l | grep puerto_rico_osm > /dev/null
+
+if [ $? -ne 0 ];
+then
+  echo  "* Creating database 'puerto_rico_osm'"
+  createdb puerto_rico_osm
+  echo "create extension postgis" | psql puerto_rico_osm
+  osm2pgsql  -d puerto_rico_osm extractions/puerto-rico.osm
+else
+  echo "* DB puerto_rico_osm already exists! skipping creation"
+fi
+
